4bb01b33f5bf0089349f43f86249295797c2c2c8
Lukasz-Cichoracki <bukasz1500@gmail.com>
2023-03-31 21:13:06 +0200

    Added crouching system

--- BootlegInferno1/Assets/Scripts/Player.cs
+++ BootlegInferno1/Assets/Scripts/Player.cs
@@ -9,12 +9,17 @@ public class Player : InputProvider
     public static Player Instance { get; private set; }
 
     private Rigidbody2D playerRigidbody;
-    private BoxCollider2D playerBoxCollider;
+    private float defaultPlayerLinearDrag;
 
     [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private float jumpForce = 1f;
-    [SerializeField] private float movementSpeed = 3f;
+    [SerializeField] private float maxMovementSpeed = 3f;
 
+    private BoxCollider2D playerBoxCollider;
+    private Vector3 boxColliderSize;
+    private Vector3 boxColliderOffset;
+
+    private bool canMove = true;
     private bool isTurnedRight;
 
     private void Start()
@@ -28,18 +33,36 @@ public class Player : InputProvider
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerBoxCollider = GetComponent<BoxCollider2D>();
 
+        boxColliderSize = playerBoxCollider.size;
+        boxColliderOffset = playerBoxCollider.offset;
+
+        defaultPlayerLinearDrag = playerRigidbody.drag;
+
+        Debug.Log(boxColliderSize);
         playerInputActions.Player.Jump.performed += Jump;
+
     }
+
+
 
     private void FixedUpdate()
     {
         Vector2 movementVector2D = new Vector2();
         float moveDirection = playerInputActions.Player.Move.ReadValue<float>();
+        float movementSpeed = maxMovementSpeed;
 
-        movementVector2D.x = moveDirection * movementSpeed;
+        Crouching();
 
-        playerRigidbody.velocity += movementVector2D;
+        if (!isGrounded())
+            movementSpeed /= 2;
 
+        if(canMove)
+        {
+            movementVector2D.x = moveDirection * movementSpeed;
+
+            playerRigidbody.velocity += movementVector2D;
+        }
+   
         if (moveDirection != 0)
         {
             TurnPlayer(moveDirection < 1f);
@@ -48,11 +71,32 @@ public class Player : InputProvider
 
     private void Jump(InputAction.CallbackContext context)
     {
-        if(isGrounded())
+        if (isGrounded())
         {
             Vector2 jumpVector2D = new Vector2(0f, 1f * jumpForce);
             if (context.performed)
                 playerRigidbody.velocity += jumpVector2D;
+        }
+    }
+
+    private void Crouching()
+    {
+        float isCrouching = playerInputActions.Player.Crouch.ReadValue<float>();
+
+        if (isCrouching != 0)
+        {
+            canMove = false;
+            float crouchingColliderSizeY = boxColliderSize.y / 2;
+            Vector3 crouchColliderVector = new Vector3(boxColliderSize.x, crouchingColliderSizeY, boxColliderSize.z);
+            float crouchingLinearDrag = defaultPlayerLinearDrag /20;
+            playerRigidbody.drag = crouchingLinearDrag;
+            playerBoxCollider.size = crouchColliderVector;
+        }
+        else
+        {
+            canMove = true;
+            playerBoxCollider.size = boxColliderSize;
+            playerRigidbody.drag = defaultPlayerLinearDrag;
         }
     }
 
@@ -70,7 +114,7 @@ public class Player : InputProvider
     private bool isGrounded()
     {
         float extraHeightTest = 0.1f;
-        RaycastHit2D raycastHit = Physics2D.Raycast(playerBoxCollider.bounds.center, Vector2.down, playerBoxCollider.bounds.extents.y+extraHeightTest,platformLayerMask);
+        RaycastHit2D raycastHit = Physics2D.Raycast(playerBoxCollider.bounds.center, Vector2.down, playerBoxCollider.bounds.extents.y + extraHeightTest, platformLayerMask);
         return raycastHit;
     }
 }
